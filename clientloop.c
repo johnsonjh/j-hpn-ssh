060d91ae77882aa4d34f8274bdf201e46a2ce519
Jeff Johnson <jeff@gridfinity.com>
2021-02-26 04:41:07 -0500

    Revert "Backport 332f21537293d66508f7342dc643bc7fe45f0f69"
    This reverts commit b3cc29b13e9dcaff539331f61ebe8af819d586f3.

--- clientloop.c
+++ clientloop.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: clientloop.c,v 1.347 2020/10/03 08:12:59 djm Exp $ */
+/* $OpenBSD: clientloop.c,v 1.346 2020/09/16 03:07:31 dtucker Exp $ */
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
  * Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
@@ -2245,17 +2245,7 @@ client_input_hostkeys(struct ssh *ssh)
 	debug3("%s: %zu keys from server: %zu new, %zu retained. %zu to remove",
 	    __func__, ctx->nkeys, ctx->nnew, ctx->nkeys - ctx->nnew, ctx->nold);
 
-	if (ctx->complex_hostspec && (ctx->nnew != 0 || ctx->nold != 0)) {
-		debug("%s: manual list or wildcard host pattern found, "
-		    "skipping UserKnownHostsFile update", __func__);
-		goto out;
-	} else if (sshkey_type_is_cert(ssh->kex->hostkey_type) &&
-	    ctx->ca_available &&
-	    (ssh->kex->flags & KEX_HOSTCERT_CONVERT) == 0) {
-		debug("%s: server offered certificate host key, "
-		    "skipping UserKnownHostsFile update", __func__);
-		goto out;
-	} else if (ctx->nnew == 0 && ctx->nold != 0) {
+	if (ctx->nnew == 0 && ctx->nold != 0) {
 		/* We have some keys to remove. Just do it. */
 		update_known_hosts(ctx);
 	} else if (ctx->nnew != 0) {
